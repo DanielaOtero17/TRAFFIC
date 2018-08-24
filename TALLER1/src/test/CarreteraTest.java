81f52bc4cd96a61d3d4da2a4e64e1b0d6bf79b82
1193260391 <1193260391@SC203E25.academico.icesi.edu.co>
2018-08-24 13:08:44 -0500

    body of test

---
+++ TALLER1/src/test/CarreteraTest.java
@@ -0,0 +1,56 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CarreteraTest {
+
+	public void setupStage1() {
+		
+	}
+	
+
+	public void setupStage2() {
+		
+	}
+
+
+	public void setupStage3() {
+		
+	}
+
+
+	public void setupStage4() {
+		
+	}
+
+
+	@Test
+	void testAgregarAutomovil() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAMoverVehiculo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testContarVehiculosEnMovimiento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testContarAutosTranscurridos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAgrContarMovimiento() {
+		fail("Not yet implemented");
+	}
+	
+	
+
+}
