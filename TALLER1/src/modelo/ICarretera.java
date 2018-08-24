c42bf9e5cf9927926732519f5aa547c5d4bf0379
1144095154 <1144095154@SC203E22.academico.icesi.edu.co>
2018-08-24 13:14:27 -0500

    Ready Icarretera class

--- TALLER1/src/modelo/ICarretera.java
+++ TALLER1/src/modelo/ICarretera.java
@@ -1,5 +1,17 @@
 package modelo;
 
 public interface ICarretera {
+	
+	
+	public boolean agregarAutomovil (IVehiculo vehi);
+	
+	public boolean moverVehiculo();
+	
+	public int contarvehiculosEnMovimientos();
+	
+	public int contarAutosTranscurridos();
+	
+	public int contarMovimientos();
+	
 
 }
