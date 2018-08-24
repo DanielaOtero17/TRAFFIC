b6cc5c6e6ae89aaaeb94bdd689bd7b3b378eb8a4
1193260391 <1193260391@SC203E25.academico.icesi.edu.co>
2018-08-24 13:15:54 -0500

    necesary commit

---
+++ TALLER1/src/test/CarreteraTest2.java
@@ -0,0 +1,57 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CarreteraTest2 {
+
+	public void setupStage1() {
+		
+	}
+	
+
+	public void setupStage2() {
+		
+	}
+
+
+	public void setupStage3() {
+		
+	}
+
+
+	public void setupStage4() {
+		
+	}
+
+
+	@Test
+	void testAgregarAutomovil() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAMoverVehiculo() {
+	
+	}
+
+	@Test
+	void testContarVehiculosEnMovimiento() {
+	
+	}
+
+	@Test
+	void testContarAutosTranscurridos() {
+		
+	}
+
+	@Test
+	void testAgrContarMovimiento() {
+		
+	}
+	
+	
+	
+
+}
